c30fa1e3dd7b7a10c9168f6ea6f95a98365d56c7
SirTargino <joaotarginodev@gmail.com>
2026-05-23 17:59:18 -0300

    feat: adicionando novas funcionalidades ao main

--- Main.hs
+++ Main.hs
@@ -2,11 +2,31 @@ module Main where
 
 import Map
 import Util.DrawMap
+import Util.DrawInMap
+import Util.CleanTerminal
+import Core
+import System.IO
 
 main :: IO ()
 main = do
+    hSetBuffering stdout NoBuffering
+    hSetBuffering stdin LineBuffering
+    hSetEcho stdin True
+
+
+    cleanTerminal
+
     putStrLn "Carregando a arena de Bomberman..."
     estadoInicial <- loadStaticMap "assets/level1.txt"
-    
-    putStrLn "\n--- Sucesso ao Inicializar ---"
+
+    cleanTerminal
     drawMap estadoInicial
+
+    let initialPosition = (1,1) -- assumindo que sempre começa em 1,1
+
+    drawInMap initialPosition '☻'
+
+    drawInMap (0, 7) ' '
+    putStr "Use W, A, S, D + Enter para andar. 'q' + Enter para sair.\nInput: "
+
+    core initialPosition estadoInicial
